aa81a073a35709cf2874684cfe3df8710f4ce34c
Alex Truby <alex.truby15@gmail.com>
2025-03-15 21:30:40 -0700

    Add scripts to confirm ingest & delete object.

---
+++ src/confirm_doc_ingest.py
@@ -0,0 +1,17 @@
+import weaviate
+import os
+
+# Initialize Weaviate client
+client = weaviate.connect_to_local(
+    headers={
+        "X-OpenAI-Api-Key": os.environ["OPENAI_APIKEY"]  # Ensure your API key is set
+    }
+)
+
+try:
+    collection = client.collections.get("PolicyDocument")
+    for item in collection.iterator():
+        print(item.uuid, item.properties['title'])
+
+finally:
+    client.close()
---
+++ src/delete_object.py
@@ -0,0 +1,15 @@
+import weaviate
+
+# Initialize Weaviate client
+client = weaviate.connect_to_local()
+
+uuid_to_delete = input("Enter uuid of the object you want to delete: ").strip()
+
+try:
+    collection = client.collections.get("PolicyDocument")
+    collection.data.delete_by_id(
+        uuid_to_delete
+    )
+    print("Successfully delete object with uuid: {uuid_to_delete}")
+finally: 
+    client.close()
